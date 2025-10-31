287856b82cc19cf522c044cf8f748e0396bd3cbc
ashraf <ashrafdcc1502@gmail.com>
2025-10-31 21:06:45 +0600

    fixed

--- blog/settings.py
+++ blog/settings.py
@@ -15,7 +15,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-!^i=kyi-d#0g+fk-671w7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['Blog-website.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
